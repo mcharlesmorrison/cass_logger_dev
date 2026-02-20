e2bd59d68a866cb5df7200c8a2c7bd3d4e86c4bc
Matt Morrison <mcharlesmorrison@berkeley.edu>
2026-02-19 16:16:07 -0800

    bug fix

--- src/rtc_sync_example.py
+++ src/rtc_sync_example.py
@@ -1,11 +1,12 @@
-import cass_download
+import src.cass_commands as cass_commands
 
 
 # example for setting and getting RTC time
 def main():
-    print(cass_download.get_RTC_time())
-    print(f"RTC Success = {cass_download.set_RTC_time()}")
-    print(cass_download.get_RTC_time())
+    cass_util = cass_commands.CassCommands()
+    print(cass_util.get_RTC_time())
+    print(f"RTC Success = {cass_commands.set_RTC_time()}")
+    print(cass_util.get_RTC_time())
 
 
 if __name__ == "__main__":
